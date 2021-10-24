ebb1803f49fb6e1936187416518fc15fcdcc8c95
Gopika Murali <gopikamurali.mec@gmail.com>
2021-10-24 15:02:40 +0530

    Corrected paths

--- BookShop_Frontend/bookstore-frontend-ng-app/src/app/app.module.ts
+++ BookShop_Frontend/bookstore-frontend-ng-app/src/app/app.module.ts
@@ -3,20 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { MenuComponent } from './menu/menu.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { MenuComponent } from './shared/menu/menu.component';
 import { HomeComponent } from './home/home.component';
-import { BooksComponent } from './books/books.component';
-import { ShoppingcartComponent } from './shoppingcart/shoppingcart.component';
-import { ListBooksComponent } from './books/list-books/list-books.component';
-import { BookDetailsComponent } from './books/book-details/book-details.component';
-import { LoginComponent } from './auth/login/login.component';
-import { SignupComponent } from './auth/signup/signup.component';
-import { ResetPwComponent } from './auth/reset-pw/reset-pw.component';
-import { CartComponent } from './shopping/cart/cart.component';
-import { CheckoutComponent } from './shopping/checkout/checkout.component';
-import { ConfirmationComponent } from './shopping/confirmation/confirmation.component';
+
+import { ListBooksComponent } from './books/components/list-books/list-books.component';
+import { BookDetailsComponent } from './books/components/book-details/book-details.component';
+import { LoginComponent } from './auth/components/login/login.component';
+import { SignupComponent } from './auth/components/signup/signup.component';
+import { ResetPwComponent } from './auth/components/reset-pw/reset-pw.component';
+import { CartComponent } from './shopping/components/cart/cart.component';
+import { CheckoutComponent } from './shopping/components/checkout/checkout.component';
+
 
 @NgModule({
   declarations: [
@@ -25,8 +24,8 @@ import { ConfirmationComponent } from './shopping/confirmation/confirmation.comp
     FooterComponent,
     MenuComponent,
     HomeComponent,
-    BooksComponent,
-    ShoppingcartComponent,
+   
+  
     ListBooksComponent,
     BookDetailsComponent,
     LoginComponent,
@@ -34,7 +33,6 @@ import { ConfirmationComponent } from './shopping/confirmation/confirmation.comp
     ResetPwComponent,
     CartComponent,
     CheckoutComponent,
-    ConfirmationComponent
   ],
   imports: [
     BrowserModule,
