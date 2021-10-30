d675afd8a27c7e584204f5ee10b77500fbc3b092
Arun <arunhari3210@gmail.com>
2021-10-30 19:25:42 +0530

    Added wishlist and discount coupon controllers

---
+++ BookShop_Backend/Controllers/DiscountCouponsController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using BookShop_Backend.Models;
+
+namespace BookShop_Backend.Controllers
+{
+    [RoutePrefix("api/DiscountCoupons")]
+    public class DiscountCouponsController : ApiController
+    {
+        private ApplicationDBContext db = new ApplicationDBContext();
+
+        // GET: api/DiscountCoupons
+        [Route("")]
+        public IQueryable<DiscountCoupon> GetDiscountCoupons()
+        {
+            return db.DiscountCoupons;
+        }
+
+        // GET: api/DiscountCoupons/5
+        [Route("{id:int}")]
+        [ResponseType(typeof(DiscountCoupon))]
+        public IHttpActionResult GetDiscountCoupon(int id)
+        {
+            DiscountCoupon discountCoupon = db.DiscountCoupons.Find(id);
+            if (discountCoupon == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(discountCoupon);
+        }
+
+        // PUT: api/DiscountCoupons/5
+        [Route("{id:int}")]
+        [ResponseType(typeof(DiscountCoupon))]
+        public IHttpActionResult PutDiscountCoupon(int id, DiscountCoupon discountCoupon)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != discountCoupon.id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(discountCoupon).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DiscountCouponExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return Ok(discountCoupon);
+        }
+
+        // POST: api/DiscountCoupons
+        [Route("")]
+        [ResponseType(typeof(DiscountCoupon))]
+        public IHttpActionResult PostDiscountCoupon(DiscountCoupon discountCoupon)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.DiscountCoupons.Add(discountCoupon);
+            db.SaveChanges();
+
+            return Ok(discountCoupon);
+        }
+
+        // DELETE: api/DiscountCoupons/5
+        [Route("{id:int}")]
+        [ResponseType(typeof(DiscountCoupon))]
+        public IHttpActionResult DeleteDiscountCoupon(int id)
+        {
+            DiscountCoupon discountCoupon = db.DiscountCoupons.Find(id);
+            if (discountCoupon == null)
+            {
+                return NotFound();
+            }
+
+            db.DiscountCoupons.Remove(discountCoupon);
+            db.SaveChanges();
+
+            return Ok(discountCoupon);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool DiscountCouponExists(int id)
+        {
+            return db.DiscountCoupons.Count(e => e.id == id) > 0;
+        }
+    }
+}
---
+++ BookShop_Backend/Controllers/WishlistsController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using BookShop_Backend.Models;
+
+namespace BookShop_Backend.Controllers
+{
+    [RoutePrefix("api/Wishlist")]
+    public class WishlistsController : ApiController
+    {
+        private ApplicationDBContext db = new ApplicationDBContext();
+
+        // GET: api/Wishlist
+        [Route("")]
+        public IEnumerable<Wishlist> GetWishlist()
+        {
+            int userId = 2;
+            var wishlist = (from item in db.Wishlist
+                              where item.userId == userId
+                              select item).ToList();
+            return wishlist;
+        }
+
+        // POST: api/Wishlists
+        [Route("")]
+        [ResponseType(typeof(Wishlist))]
+        public IHttpActionResult PostWishlist(Wishlist wishlist)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Wishlist.Add(wishlist);
+            db.SaveChanges();
+
+            return Ok(wishlist);
+        }
+
+        // DELETE: api/Wishlists/5
+        [Route("{id:int}")]
+        [ResponseType(typeof(Wishlist))]
+        public IHttpActionResult DeleteWishlist(int id)
+        {
+            Wishlist wishlist = db.Wishlist.Find(id);
+            if (wishlist == null)
+            {
+                return NotFound();
+            }
+
+            db.Wishlist.Remove(wishlist);
+            db.SaveChanges();
+
+            return Ok(wishlist);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool WishlistExists(int id)
+        {
+            return db.Wishlist.Count(e => e.id == id) > 0;
+        }
+    }
+}
