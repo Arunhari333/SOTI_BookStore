8252c4ce214010775ccad963d54c3beee2a315e2
Gopika Murali <gopikamurali.mec@gmail.com>
2021-10-31 17:39:21 +0530

    Added admin module and books component in frontend

---
+++ BookShop_Frontend/bookstore-frontend-ng-app/src/app/admin/admin-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BooksComponent } from './components/books/books.component';
+import { RouterModule } from '@angular/router';
+
+//configuring routes
+const adminRoutes = [
+  { path: '', component:  },
+  { path: '/books', component: BooksComponent }
+]
+//register routes
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(adminRoutes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AdminRoutingModule { }
