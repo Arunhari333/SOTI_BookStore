cbaa4e6ee24ec675c5222e3f923d8c0fce64fd9c
Vijaya Raghavendra <vijaya.raghavendra@soti.net>
2021-11-08 18:48:49 +0530

    Modified Footer

--- BookShop_Frontend/bookstore-frontend-ng-app/src/app/shared/footer/footer.component.ts
+++ BookShop_Frontend/bookstore-frontend-ng-app/src/app/shared/footer/footer.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { DataStoreService } from '../services/data-store.service';
+import { HeaderService } from '../services/header.service';
 
 @Component({
   selector: 'app-footer',
@@ -6,10 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-
-  constructor() { }
+  categories: any[]=[];
+  isAdmin: any;
+  isloggedIn: any;
+  constructor(private router: Router, private headerService:HeaderService,
+    private dataStore:DataStoreService) { }
 
   ngOnInit(): void {
+    this.dataStore.isAdmin.subscribe(isAdmin => {
+      console.log(isAdmin);
+      this.isAdmin = isAdmin;
+    });
+    this.headerService.getCategories()
+      .subscribe((res: any) => {
+        console.log(res);
+        this.categories = res;
+      });
+      this.isloggedIn = localStorage.getItem('authToken');
+      this.dataStore.categoryId.subscribe(id => console.log(id));
   }
-
+  handleCategoryClick(id: string){
+    this.dataStore.changeCategoryId(id);
+  }
+  
 }
